c01b0ead12c4b5b6fa994e4f8fcde1f2e8309774
Robinson Ordonez <robinsonordonezbarreiro@gmail.com>
2026-02-24 14:00:44 -0500

    Ajustes

--- src/config/global.js
+++ src/config/global.js
@@ -247,7 +247,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/XX_CF02_DU.pdf',
+        download: 'downloads/72312200_CF02_DU.pdf',
       },
       {
         icono: 'fas fa-download',
